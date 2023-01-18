47e845d778cddb01c72e1d1b5f10bd2d821c56bc
pratyashha <pratyasha.616@gmail.com>
2023-01-18 20:19:04 +0530

    Link Changed

--- client/script.js
+++ client/script.js
@@ -74,7 +74,7 @@ function typeText(element, text) {
   loader(messageDiv);
 
  //fetch data from server
-const response = await fetch('http://localhost:5000',{
+const response = await fetch('https://codex-05x6.onrender.com',{
   method:'POST',
   headers:{
    'Content-Type':'application/json' 
